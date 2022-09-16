b26ac7df0c09d06ce819ebc72cf8b86732e18e4f
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-09-16 23:12:54 +0100

    added the relationship between the models

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+import User from "./user.model";
+import Friend from "./friend.model";
+import Account from "./account.model";
+
+User.hasOne(Account, {
+  foreignKey: {
+    type: DataTypes.UUID
+  }
+})
+
+Account.belongsTo(User)
+User.hasMany(Friend, {
+  foreignKey: {
+    type: DataTypes.UUID,
+  },
+});
+Friend.belongsTo(User)
