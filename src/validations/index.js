78b9069b060a90d60e7b7734b6a7138d7fd1fe2d
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-09-17 13:34:51 +0100

    added friends validations

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,6 +1,6 @@
 import UserValidations from './user';
 import AccountValidations from './account';
-import FriendValidations from './account';
+import FriendValidations from './friend';
 
 const UserAPIValidations = new UserValidations();
 const AccountAPIValidations = new AccountValidations();
