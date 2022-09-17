c83dbaaa08d10cec86291cb5dfbb17e73dd58dcf
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-09-17 09:59:10 +0100

    fixed bug in friend controller

--- src/routes/friend/friend.controller.js
+++ src/routes/friend/friend.controller.js
@@ -21,14 +21,16 @@ class Test {
       let account = await findAccount({ accountId });
       let user = await findUser({ userId });
 
+      console.log(user, account)
+
       if (!account) return errorResponse(res, 'Account does not exist', 400, ':-(');
       if (!user) return errorResponse(res, 'User does not exist', 400, ':-(');
-      if (user._id === account.userId) return errorResponse(res, 'You own this account', 500, ':-(');
+      if (user._id.toString() === account.userId.toString()) return errorResponse(res, 'You own this account', 500, ':-(');
 
       const newFriend = await create({
         accountId,
         userId,
-        fullName: `&{user.firstName} ${user.lastName}`,
+        fullName: `${user.firstName} ${user.lastName}`,
         isFavorite: false,
         canViewStatus: false,
       });
@@ -53,8 +55,7 @@ class Test {
 
   async update(req, res) {
     try {
-      const friend = await find({ id: req.query.id });
-      console.log(req.body, 'req body');
+      const friend = await find(req.query.id);
       if (!friend) return res.status(404).send({ Message: 'friend does not exist' });
 
       await update(friend, req.body);
